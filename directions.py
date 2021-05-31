d1ddf3aeebc9e753287a9950bbc313fed7dc5f39
hichoe95 <hichoe95@naver.com>
2021-05-31 18:14:30 +0900

    2021

--- directions.py
+++ directions.py
@@ -7,9 +7,9 @@ import torch
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 import copy
-u
+
 from misc import *
-#dkdkdkdk
+
 device = 'cuda:4' if torch.cuda.is_available() else 'cpu'
 
 latent_z, latent_w = latents()
